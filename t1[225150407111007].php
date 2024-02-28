bc176b639770113b64f00997952e9a462d9ad68b
daffy2624 <133834195+daffy2624@users.noreply.github.com>
2024-02-28 17:03:38 +0700

    Add files via upload

---
+++ t1[225150407111007].php
@@ -0,0 +1,13 @@
+<?php
+
+// Deklarasi variabel
+$nim = "225150407111007";
+$nama = "Muhammad Daffy Hamami";
+
+// Perulangan untuk menampilkan nim dan nama
+for ($i=0; $i<5; $i++) {
+  echo "NIM: " . $nim . "<br>";
+  echo "Nama: " . $nama . "<br><br>";
+}
+
+?>
